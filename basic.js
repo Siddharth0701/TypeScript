63d8ebbb892e132d2ca25b33ff60b7b3e7d936f7
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-10-20 15:58:02 +0530

    Added Array

--- basic.js
+++ basic.js
@@ -13,3 +13,16 @@ var persion1 = {
     name: 'Singh',
     age: 21
 };
+//Array
+var person = {
+    name: 'siddhart',
+    age: 21,
+    hobbies: ['spots', 'Cooking']
+};
+var favoriteActivity;
+favoriteActivity = ['spots'];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobbies = _a[_i];
+    console.log(hobbies.toUpperCase());
+}
+console.log(person);
--- basic.ts
+++ basic.ts
@@ -18,3 +18,17 @@ const persion1={
     name:'Singh',
     age:21
 };
+
+//Array
+const person={
+    name:'siddhart',
+    age:21,
+    hobbies:['spots','Cooking']
+};
+let favoriteActivity:string[];
+favoriteActivity=['spots'];
+for(const hobbies of person.hobbies){
+    console.log(hobbies.toUpperCase());
+}
+
+console.log(person);
