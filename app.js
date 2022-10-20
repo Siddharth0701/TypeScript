2da53cb97d56f82c9010fb91319c25223aac4587
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-10-20 15:12:48 +0530

    Data types

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("Hello Siddharth Singh");
+function add(n1, n2, showresult) {
+    console.log(typeof number1);
+    if (showresult) {
+        console.log(n1 + n2);
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 9.5;
+var printresult = true;
+//const result= add(number1, number2,printresult);
+add(number1, number2, printresult);
+//console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,21 @@
 console.log("Hello Siddharth Singh");
+function add(n1:number,n2:number,showresult:boolean) {
+    console.log(typeof number1);
+    if(showresult){
+        console.log(n1+n2);
+        
+    }
+    else{
+
+        return n1+n2;
+    }
+    
+    
+    
+}
+const number1=5;
+const number2=9.5;
+const printresult=true;
+//const result= add(number1, number2,printresult);
+add(number1, number2,printresult);
+//console.log(result);
