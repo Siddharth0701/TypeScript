953250d94fbea9fef929b745bba2cb44003a8041
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-10-21 12:15:16 +0530

    Unknown types

---
+++ unknown.js
@@ -0,0 +1,8 @@
+var userinput;
+//let userinput:any;
+var userName;
+userinput = 6;
+userinput = 'Siddharth';
+if (typeof userinput === 'string') {
+    userName = userinput;
+}
---
+++ unknown.ts
@@ -0,0 +1,9 @@
+let userinput:unknown;
+//let userinput:any;
+let userName:string;
+userinput=6;
+userinput='Siddharth';
+if( typeof userinput==='string'){
+    userName=userinput;
+
+}
