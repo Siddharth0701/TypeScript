9772dabb258fd11d6f7710cce1b00a59288594a2
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-10-21 12:05:33 +0530

    Callback function

--- function.js
+++ function.js
@@ -4,6 +4,10 @@ function add(n1, n2) {
 function printResult(num) {
     console.log('Result:' + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(10, 20));
 //let combinedvalue:Function;
 var combinedvalue;
@@ -11,3 +15,6 @@ combinedvalue = add;
 //combinedvalue=printResult;
 //combinedvalue=5;
 console.log(combinedvalue(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
--- function.ts
+++ function.ts
@@ -6,6 +6,12 @@ function printResult(num:number):void {
     console.log('Result:'+num);
     
     
+    
+}
+function addAndHandle(n1:number,n2:number,cb:(num:number)=>void) {
+    const result=n1+n2;
+    cb(result);
+    
 }
 printResult(add(10,20));
 //let combinedvalue:Function;
@@ -15,3 +21,7 @@ combinedvalue    =add;
 //combinedvalue=5;
 
 console.log(combinedvalue(8,8));
+addAndHandle(10,20,(result)=>{
+    console.log(result);
+    
+});
