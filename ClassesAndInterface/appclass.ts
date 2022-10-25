0958279b7f33e68453ed41f58ffd2cb8e5500f4c
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-10-25 11:15:44 +0530

    This keywords

--- ClassesAndInterface/appclass.ts
+++ ClassesAndInterface/appclass.ts
@@ -4,9 +4,15 @@ class Department{
         this.name=n;
 
     }
-    
+    describe() {
+        console.log('Department:'+this.name);
+        
+
+    }
 }
 
   const accounting=new Department("Accounting");
-  console.log(accounting);
+  accounting.describe();
+  const accountingCopy={describe:accounting.describe};
+  //console.log(accounting);
   
