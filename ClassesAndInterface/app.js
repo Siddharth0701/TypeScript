00ba87be75365cfe2ebce502d13f18efbf667e15
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-10-25 17:19:54 +0530

    interfaces with classes

--- ClassesAndInterface/app.js
+++ ClassesAndInterface/app.js
@@ -1,10 +1,20 @@
 "use strict";
-let user1;
-user1 = {
-    name: 'Siddharth',
-    age: 21,
-    greet(phrase) {
-        console.log(phrase + '' + this.name);
+class Person {
+    constructor(n) {
+        this.age = 21;
+        this.name = n;
     }
-};
+    greet(phrase) {
+    }
+}
+let user1;
+user1 = new Person('Siddharth');
+//{
+//     name:'Siddharth',
+//     //age:21,
+//     greet(phrase:string){
+//         console.log(phrase+''+this.name);
+//     }
+// };
 user1.greet('Hi there - I am ');
+console.log(user1);
--- ClassesAndInterface/app.ts
+++ ClassesAndInterface/app.ts
@@ -1,15 +1,31 @@
-interface Person{
+interface Greetable{
     name:string;
-    age:number;
+    // age:number;
     greet(phrase:string):void;
 }
-let user1:Person;
-user1={
-    name:'Siddharth',
-    age:21,
-    greet(phrase:string){
-        console.log(phrase+''+this.name);
+class Person implements Greetable{
+    name: string;
+    age=21;
+
+    constructor( n:string) {
+        this.name=n;
         
     }
-};
+    greet(phrase: string) {
+        
+    }
+
+
+}
+let user1:Greetable;
+user1= new Person('Siddharth');
+//{
+//     name:'Siddharth',
+//     //age:21,
+//     greet(phrase:string){
+//         console.log(phrase+''+this.name);
+        
+//     }
+// };
 user1.greet('Hi there - I am ');
+console.log(user1);
