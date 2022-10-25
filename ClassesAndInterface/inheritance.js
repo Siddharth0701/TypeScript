fb117512a8d25e9463fba67b9b2af8a410597d6b
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-10-25 15:07:59 +0530

    inheritance

---
+++ ClassesAndInterface/inheritance.js
@@ -0,0 +1,53 @@
+"use strict";
+class DepartmentIT {
+    constructor(name, id) {
+        //this.name=na;
+        //this.id=id;
+        this.name = name;
+        this.id = id;
+        // private id:string;
+        // private name:string;
+        this.employees = [];
+    }
+    describe() {
+        console.log(`Department(${this.name}):${this.id}`);
+    }
+    addEmployee(employee) {
+        // this.id='d1';
+        this.employees.push(employee);
+    }
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+class ITDepartment extends DepartmentIT {
+    constructor(id, admin) {
+        super(id, 'it');
+        this.admin = admin;
+    }
+}
+class AccountingDepartment extends DepartmentIT {
+    constructor(id, report) {
+        super(id, 'IT');
+        this.report = report;
+    }
+    addReport(text) {
+        this.report.push(text);
+    }
+    getReport() {
+        console.log(this.report);
+    }
+}
+const account = new ITDepartment('D1', ['Max']);
+//
+const accountingit = new DepartmentIT('D1', 'Singh');
+account.addEmployee('SIDDHARTH');
+account.addEmployee('Singh');
+//accounting1.employees[2]='Mohan';
+account.describe();
+console.log(account);
+const accounti = new AccountingDepartment('d2', []);
+accounti.addReport('Something went wrong...');
+accounti.getReport();
+//accounting1.printEmployeeInformation();
---
+++ ClassesAndInterface/inheritance.ts
@@ -0,0 +1,63 @@
+class DepartmentIT{
+    // private id:string;
+    // private name:string;
+    private employees:string[]=[];
+     constructor( public name:string, private readonly id:string){
+         //this.name=na;
+         //this.id=id;
+ 
+     }
+     describe( this:DepartmentIT) {
+         console.log(`Department(${this.name}):${this.id}`);
+         
+ 
+     }
+     addEmployee( employee:string){
+       // this.id='d1';
+        this.employees.push(employee); 
+     }
+     printEmployeeInformation(){
+         console.log(this.employees.length);
+         console.log(this.employees);
+         
+     }
+ }
+ class ITDepartment extends DepartmentIT{
+    admin:string[];
+    constructor( id:string, admin:string[]){
+        super(id,'it');
+        this.admin=admin;
+
+
+    }
+
+ }
+ class AccountingDepartment extends DepartmentIT{
+
+    constructor( id:string , private report:string[]){
+        super(id,'IT');
+    
+    }
+    addReport(text:string){
+        this.report.push(text);
+    }
+    getReport(){
+        console.log(this.report);
+        
+    }
+
+ }
+ const account=new ITDepartment('D1',['Max'])
+   //
+
+   const accountingit=new DepartmentIT('D1','Singh');
+   account.addEmployee('SIDDHARTH');
+ account.addEmployee('Singh');
+ //accounting1.employees[2]='Mohan';
+   account.describe();
+   console.log(account);
+   const accounti=new AccountingDepartment('d2',[]);
+   accounti.addReport('Something went wrong...');
+   accounti.getReport();
+   
+   //accounting1.printEmployeeInformation();
