1c3a79cc05c3722bec70e78511173cceb04ea74d
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-10-25 20:29:02 +0530

    More on Type Guards

--- AdvancedTopicInTS/app.ts
+++ AdvancedTopicInTS/app.ts
@@ -1,7 +1,7 @@
 //InterSection
 type Admin={
     name:string;
-    privillage:string[];
+    privileges:string[];
 
 };
 type Employe={
@@ -11,9 +11,66 @@ type Employe={
 type ElevateEmployee=Admin & Employe;
 const e1:ElevateEmployee={
     name:'Singh',
-    privillage:['create-server'],
+    privileges:['create-server'],
     startDate: new Date()
 };
 type Combinable =string | number;
 type Numeric =number | boolean;
 type Universal=Combinable & Numeric
+
+//More On Type Guards
+function add2(a:Combinable, b:Combinable) {
+    if( typeof a=== 'string' || typeof b === 'string'){
+        return a.toString() + b.toString();
+
+    }
+    return a +b;
+    
+}
+type UnknownEmployee =Employe | Admin;
+function printEmployeeInformation(emp:UnknownEmployee) {
+    console.log('Name'+emp.name);
+    if('privileges' in emp ){
+        console.log('Privillage:'+emp.privileges);
+
+    }
+    if('startDate' in emp){
+        console.log('Privillage:'+emp.startDate);
+        
+    }
+   
+    
+    
+}
+printEmployeeInformation({name:'Manu', startDate:new Date()});
+class Car{
+    drive(){
+        console.log('Driving...');
+        
+    }
+}
+
+class Truck{
+    drive(){
+    console.log("Driving a truck...");
+    }
+    loadCargo(amount:number){
+        console.log('loading cargo ...'+amount);
+    
+    }
+}
+type Vehicle= Car | Truck;
+const v1=new Car();
+const v2=new Truck();
+function useVehicle(vehicle:Vehicle) {
+    vehicle.drive();
+    // if('loadCargo' in vehicle){
+    //     vehicle.loadCargo(1000);
+    // }    
+    if(vehicle instanceof Truck){
+        vehicle.loadCargo(1000);
+    }    
+}
+useVehicle(v1);
+useVehicle(v2);
+
