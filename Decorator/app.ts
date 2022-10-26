c26b0f46fc8601abe218f6248e1ef635d915df92
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-10-26 16:28:54 +0530

    Decorator factories

--- Decorator/app.ts
+++ Decorator/app.ts
@@ -1,9 +1,19 @@
-function Logger( constructor:Function) {
-     console.log('Logging..');
-     console.log(constructor);
+// function Logger( constructor:Function) {
+//      console.log('Logging..');
+//      console.log(constructor);
      
+// }
+function Logger(LogString:string ) {
+    return function(constructor:Function){
+        console.log(LogString);
+        console.log(constructor);
+        
+    }
+    
+   
+    
 }
-@Logger
+@Logger('LOGGING -PERSONSINGH')
 class PersonSingh{
     name='Singh';
 
