4649ec18d2237bce0d9a1321a9e385ca85f0257d
Siddharth0701 <siddharth.singh0701@gmail.com>
2022-11-08 16:35:12 +0530

    Added middleware and types

--- NodeJsExpressTs/src/app.ts
+++ NodeJsExpressTs/src/app.ts
@@ -1,3 +1,8 @@
-import  express from "express";
+import  express, { NextFunction } from "express";
+import  todoRoutes from './routes/todos';
 const app=express();
+app.use('/todos',todoRoutes);
+app.use((err:Error,req:express.Request,res:express.Response,next:NextFunction)=>{
+res.status(500).json({message:err.message});
+});
 app.listen(3000);
